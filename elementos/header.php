66f21987ef008e6fb4b36cce8f5860c60ad4ca70
AlejandroDure <trececerodev@gmail.com>
2025-10-06 10:20:17 +0200

    Añadida barra de navegación Bootstrap

--- elementos/header.php
+++ elementos/header.php
@@ -1,4 +1,19 @@
 <header class="text-center bg-white p-3 rounded shadow-sm">
   <h1 class="text-primary">🌟 Bienvenido a mi primer sitio PHP 🌟</h1>
   <p class="text-muted">Usando include() por primera vez</p>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mt-3">
+    <div class="container justify-content-center">
+      <ul class="navbar-nav flex-row">
+        <li class="nav-item mx-2">
+          <a class="nav-link active" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="productos.php">Productos</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="contacto.php">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </header>
